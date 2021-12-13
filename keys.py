82be9009044acb6374f99ad709a68494c998251d
unknown <15an27@queensu.ca>
2021-12-13 14:47:33 -0500

    Keys updated to new benchmark location.

--- keys.py
+++ keys.py
@@ -28,25 +28,25 @@ class NameKeys:
 
 
 class FileFolderPaths:
-    CHINESE_TRAIN_ROOT: str = '../China-TSRD/TSRD-Train-Images/'
-    CHINESE_TRAIN_ANNOTATIONS: str = '../China-TSRD/TSRD-Train-Annotation/TsignRecgTrain4170Annotation.txt'
-    CHINESE_TEST_ROOT: str = '../China-TSRD/TSRD-Test-Images/'
-    CHINESE_TEST_ANNOTATIONS: str = '../China-TSRD/TSRD-Test-Annotation/TsignRecgTest1994Annotation.txt'
+    CHINESE_TRAIN_ROOT: str = '../benchmarks/China-TSRD/TSRD-Train-Images/'
+    CHINESE_TRAIN_ANNOTATIONS: str = '../benchmarks/China-TSRD/TSRD-Train-Annotation/TsignRecgTrain4170Annotation.txt'
+    CHINESE_TEST_ROOT: str = '../benchmarks/China-TSRD/TSRD-Test-Images/'
+    CHINESE_TEST_ANNOTATIONS: str = '../benchmarks/China-TSRD/TSRD-Test-Annotation/TsignRecgTest1994Annotation.txt'
 
-    GERMAN_TRAIN_ROOT: str = '../german-GTRSD/'
-    GERMAN_TRAIN_ANNOTATIONS: str = '../german-GTRSD/Train.csv'
-    GERMAN_TEST_ROOT: str = '../german-GTRSD/'
-    GERMAN_TEST_ANNOTATIONS: str = '../german-GTRSD/Test.csv'
+    GERMAN_TRAIN_ROOT: str = '../benchmarks/german-GTRSD/'
+    GERMAN_TRAIN_ANNOTATIONS: str = '../benchmarks/german-GTRSD/Train.csv'
+    GERMAN_TEST_ROOT: str = '../benchmarks/german-GTRSD/'
+    GERMAN_TEST_ANNOTATIONS: str = '../benchmarks/german-GTRSD/Test.csv'
 
-    BELGIUM_TRAIN_ROOT: str = '../belgium-TSC/BelgiumTSC_Training/Training'
+    BELGIUM_TRAIN_ROOT: str = '../benchmarks/belgium-TSC/BelgiumTSC_Training/Training'
     BELGIUM_TRAIN_ANNOTATIONS: str = None
-    BELGIUM_TEST_ROOT: str = '../belgium-TSC/BelgiumTSC_Training/Training'
+    BELGIUM_TEST_ROOT: str = '../benchmarks/belgium-TSC/BelgiumTSC_Training/Training'
     BELGIUM_TEST_ANNOTATIONS: str = None
 
-    RUSSIAN_TRAIN_ROOT: str = '../rtsd-r1/train/'
-    RUSSIAN_TRAIN_ANNOTATIONS: str = '../rtsd-r1/gt_train.csv'
-    RUSSIAN_TEST_ROOT: str = '../rtsd-r1/test/'
-    RUSSIAN_TEST_ANNOTATIONS: str = '../rtsd-r1/gt_test.csv'
+    RUSSIAN_TRAIN_ROOT: str = '../benchmarks/rtsd-r1/train/'
+    RUSSIAN_TRAIN_ANNOTATIONS: str = '../benchmarks/rtsd-r1/gt_train.csv'
+    RUSSIAN_TEST_ROOT: str = '../benchmarks/rtsd-r1/test/'
+    RUSSIAN_TEST_ANNOTATIONS: str = '../benchmarks/rtsd-r1/gt_test.csv'
 
 
 class TableColumns:
