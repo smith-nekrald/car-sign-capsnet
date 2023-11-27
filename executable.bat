5691f6ff701617b618820894451b7518d0b24dcd
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2023-11-27 16:34:20 -0500

    Documented executable.bat

--- executable.bat
+++ executable.bat
@@ -1,1 +1,9 @@
+@echo off
+
+REM Script to create standalone executable.
+
+REM Author: Aliaksandr Nekrashevich
+REM Email: aliaksandr.nekrashevich@queensu.ca
+REM (c) Smith School of Business, 2023
+
 pyinstaller main.py
