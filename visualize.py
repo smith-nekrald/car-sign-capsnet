a3cadcca1a95ab89d839cb5532666f1a422b8471
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2023-11-26 22:34:02 -0500

    Documented visualize.py.

--- visualize.py
+++ visualize.py
@@ -1,11 +1,23 @@
+""" Visualization API. """
+
+# Author: Aliaksandr Nekrashevich
+# Email: aliaksandr.nekrashevich@queensu.ca
+# (c) Smith School of Business, 2021
+# (c) Smith School of Business, 2023
+
 import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_images_separately(images: np.array,
-        figure_name: str,
-        n_images: int) -> None:
+def plot_images_separately(images: np.array, figure_name: str, n_images: int) -> None:
+    """ Plots a sequence of images on the same figure, and saves result to a file. 
+
+    Args:
+        images: The array with images.
+        figure_name: The path to save figure.
+        n_images: Number of images.
+    """
     fig: plt.Figure; axes: plt.Axes
     fig, axes = plt.subplots(1, n_images)
     idx: int
